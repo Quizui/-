65840cf0ad25a506bc8253cfdaa49a833a82e058
sonio <asyutarif@gmail.com>
2023-12-26 01:25:01 +0900

    [Add] SonikSoundAPI

---
+++ SonikSoundAPI/SonikSoundAccesser/Listener/SonikAudioListener.h
@@ -0,0 +1,51 @@
+/*
+ * SonikAudioListener.h
+ *
+ *  Created on: 2023/12/24
+ *      Author: SONIC
+ */
+
+#ifndef LISTENER_SONIKAUDIOLISTENER_H_
+#define LISTENER_SONIKAUDIOLISTENER_H_
+
+namespace SonikAudio
+{
+	//オーディオリスナーさん
+	//現状リスナーとプレイヤーの距離のためのものなので位置情報のみ。
+	//音響などはまだまだ先だよねぇ。
+	class SonikAudioListener
+	{
+	private:
+		double PosX, PosY, PosZ;
+		double* p_PosX, p_PosY, p_PosZ;
+
+	public:
+		//コンストラクタ
+		SonikAudioListener(void);
+		//デストラクタ
+		~SonikAudioListener(void);
+
+		//ポジションのセット
+		void SetPosition(double x, double y, double z);
+		void SetPositionX(double x);
+		void SetPositionY(double y);
+		void SetPositionZ(double z);
+
+		//ポジションのゲット
+		void GetPosition(double& x, double& y, double& z);
+		void GetPosition(double*& x, double*& y, double*& z);
+		double GetPositionX(void);
+		double* GetPositionX(void);
+		double GetPositionY(void);
+		double* GetPositionY(void);
+		double GetPositionZ(void);
+		double* GetPositionZ(void);
+
+	};
+
+
+};
+
+
+
+#endif /* LISTENER_SONIKAUDIOLISTENER_H_ */
